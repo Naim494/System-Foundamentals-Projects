8ccd40df636f7968d9c19aac062e06a8b33a7dfc
Gene Stark <gene@starkeffect.com>
2018-08-13 21:20:40 -0400

    Bring in basecode from development repo.

---
+++ hw1/include/myrand.h
@@ -0,0 +1,20 @@
+/*
+ * DO NOT MODIFY THE CONTENTS OF THIS FILE.
+ * IT WILL BE REPLACED DURING GRADING
+ */
+#ifndef MYRAND_H
+#define MYRAND_H
+
+/*
+ * Sets the seed to be used as a starting point for the
+ * pseudorandom number generator.
+ */
+void mysrand(unsigned int seed);
+
+/*
+ * Returns a random number from the full range of integers
+ * representable in 32-bit, twos-complement encoding.
+ */
+int myrand32(void);
+
+#endif
