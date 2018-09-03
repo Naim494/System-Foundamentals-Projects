99469c84153bbb7ac5fee08629935404d16dded9
Gene Stark <gene@starkeffect.com>
2018-09-03 08:08:43 -0400

    Correct the bit used to test for help mode, so that it matches the HW1 document spec.

--- hw1/src/main.c
+++ hw1/src/main.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
     if(!validargs(argc, argv))
         USAGE(*argv, EXIT_FAILURE);
     debug("Options: 0x%lX", global_options);
-    if(global_options & 0x1) {
+    if(global_options & (0x1L << 63)) {
         USAGE(*argv, EXIT_SUCCESS);
     }
 
